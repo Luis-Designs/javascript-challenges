76ff21bda6015ad31630cfc535c5988ece756ffb
luis-designs <luislozano120xd@gmail.com>
2022-09-05 20:31:39 -0500

    :tada: solved exercises

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,6 @@
-const factorial = (number) => {
+const factorial = (number = 10) => {
   // your code here
+  return (number === 0) ? 1 : number * factorial(number - 1)
 }
 
 module.exports = factorial;
--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,15 @@
 const fibonacci = (n) => {
   // your code here
+  let [prev, next] = [0, 1]
+  let array = [next]
+  for (let i = 0; i < n - 1; i++) {
+    let fib = prev + next
+    array = [...array, fib]
+    prev = next
+    next = fib
+  }
+  return array
 }
+
 
 module.exports = fibonacci;
